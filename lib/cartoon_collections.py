ea06ac7c7bc40bc96413f887d2b5b7595609e16a
Emmanuel-Oite <143942455+Emmanuel-Oite@users.noreply.github.com>
2024-02-22 11:47:43 +0300

    Completed Assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,15 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, 1):
+        print(f"{i}. {dwarf}")
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + "!" for call in planeteer_calls]
 
-def summon_captain_planet():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def long_planeteer_calls():
-    pass
-
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in items:
+        if item in cheeses:
+            return item
+    return None
